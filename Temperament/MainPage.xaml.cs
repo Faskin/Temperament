e5035c65589d600ecdaeb136ad9c7cb819b304d5
Faskin <alex2002tyn@gmail.com>
2020-12-02 00:18:56 +0200

    Initial commit

---
+++ Temperament/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Temperament
+{
+    
+    public partial class MainPage : CarouselPage
+    {
+        public const string testUrl = "https://testometrika.com/personality-and-temper/questionnaire-eysenck-pen/";
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void TestButton_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync(testUrl);
+        }
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Описание", "", "OK");
+
+        }
+    }
+}
---
+++ Temperament/tempermanets.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Temperament
+{
+    public partial class tempermanets : ContentPage
+    {
+        public tempermanets()
+        {
+            InitializeComponent();
+        }
+    }
+}
